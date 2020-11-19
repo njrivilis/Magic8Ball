783f6ec122496a455fbfbda11bf0746652f3788d
NJ Rivilis <nrivilis@hps.holyoke.ma.us>
2020-11-19 15:39:07 +0000

    add JS if/else logic

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
-var answers = ["It is certain", 
-                   "It is decidedly so", 
-                   "Without a doubt", 
-                   "Yes - definitely",
-                   "You may rely on it", 
-                   "As I see it, yes", 
-                   "Most likely", 
-                   "Outlook good", 
-                   "Yes", "Signs point to yes",
-                   "It's a secret", 
-                   "My reply is no",
-                   "My sources say no", 
-                   "Outlook not so good",
-                   "Very doubtful", 
-                   "That's classified information", 
-                   "Ask again later", 
-                   "Better not tell you now",
-                   "Cannot predict now", 
-                   "Concentrate and ask again"];
+var trueNumber = Math.floor(Math.random() * 10) + 1;  // returns a random integer from 1 to 10
+
+var answers = ["Too High, Guess Again", "Too Low, Guess Again", "You got it!"];
+
+function number_test(n) {
+  var result = (n - Math.floor(n)) !== 0;
+
+  if (result)
+    return false; //has decimals
+  else
+    return true; //is an integer whole number
+}
+
+//function from https://www.w3resource.com/javascript-exercises/javascript-math-exercise-38.php
+
+
+window.onload = function () {
+  var eight = document.getElementById("eight");
+  var answer = document.getElementById("answer");
+  var eightball = document.getElementById("eight-ball");
+  var numberGuessed = document.getElementById("numberGuessed");
+
+  eightball.addEventListener("click", function () {
+
+    if (numberGuessed.value.length < 1) {
+      alert('Guess a Number!');
+    }
+
+    else if (numberGuessed.value < 1) {
+      alert('Your number must be at least 1')
+    }
+
+    else if (numberGuessed.value > 10) {
+      alert('Your number must be less than 10')
+    }
+
+    else if (!number_test(numberGuessed.value)) {
+      alert('Your number can\'t be a decimal')
+    }
+
+    else if (numberGuessed.value == trueNumber){
+            eight.innerText = "";
+      answer.innerText = answers[2];
+
+    }
+
+
+else if (numberGuessed.value > trueNumber){
+            eight.innerText = "";
+      answer.innerText = answers[0];
+
+    }
+
+else if (numberGuessed.value < trueNumber){
+            eight.innerText = "";
+      answer.innerText = answers[1];
+
+    }
+
     
-    window.onload = function() {
-       var eight = document.getElementById("eight");
-       var answer = document.getElementById("answer");
-       var eightball = document.getElementById("eight-ball");
-       var question = document.getElementById("question");
-       
-       eightball.addEventListener("click", function() {
-         if (question.value.length < 1) {
-           alert('Enter a question!');
-         } else {
-           eight.innerText = "";
-           var num = Math.floor(Math.random() * Math.floor(answers.length));
-           answer.innerText = answers[num];
-         }
-       });
-    };
+  });
+};
